2fce36931d7314210e3ad8ed3df9445e3834bfa5
aya-alrefaey <ayaalazab164@gmail.com>
2026-05-07 20:44:33 +0300

    Change database user, password, host, and name
    Updated database connection credentials for production.

--- admin/assets/inc/config.php
+++ admin/assets/inc/config.php
@@ -1,7 +1,7 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="orrsphp";
+$dbuser="if0_41821935";
+$dbpass="FYEaZDTrfcxrge";
+$host="sql208.infinityfree.com";
+$db="if0_41821935_orrsphp";
 $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
 ?>
